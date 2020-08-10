f4cc7917ef881d287315effab3713d0037bbd69d
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-10 22:38:14 +0300

    squarecheck.cpp
    проверка работы оператора степени

---
+++ squarecheck.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int x, fuckyou;
+
+int main()
+{
+	cout<<"Enter x: ";
+	cin>>x;
+	x=x^2;
+	cout<<"x^2="<<x<<endl;//это не степень, а побитовое исключающее ИЛИ
+	return fuckyou;
+}
