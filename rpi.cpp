baf9d8a2cf3a471e90678e8937ebceedbcef98f0
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-11 23:15:38 +0300

    Create rpi.cpp

---
+++ rpi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, world!"<<endl;
+	/*многострочнй комменетарий
+	 * iwant to sleep
+	 * too late with programming for today*/
+}
