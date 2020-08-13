4c217873cdff0a72692e8702cb663b94cde3315a
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-13 21:47:41 +0300

    training decrementing

---
+++ decrement.cpp
@@ -0,0 +1,19 @@
+include <iostream>
+using namespace std;
+
+int num=10;
+
+void dec()
+{
+        while (num>(-1))
+        {
+        cout<<num<<" ";
+        num--;
+        }
+        cout<<endl;
+}
+
+int main()
+{
+        dec();
+}
