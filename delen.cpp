b2e6f033b68587f09e2c40a24994eef6c2972d78
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-24 22:32:21 +0300

    a/b and a%b

---
+++ delen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Введите делимое: ";
+	cin>>a;
+	cout<<"Введите делитель: ";
+	cin>>b;
+	cout<<"Частное равно "<<a/b<<endl;
+	cout<<"Остаток от деления равен "<<a%b<<endl;
+}
