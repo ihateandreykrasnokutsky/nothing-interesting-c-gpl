a7d8a4bc231566cf32b8383435d07544154116ad
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-28 22:05:19 +0300

    Create ll.cpp

---
+++ ll.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=100000000;
+	long long b=100000000;
+	cout<<"int a="<<a*1000000<<endl;
+	cout<<"long long b="<<b*1000000000000000<<endl;
+}
