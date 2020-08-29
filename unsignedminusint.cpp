31e59c37ffe8201c28859b39febf2d1be223ffbc
Andrey Krasnokutsky <gef711@gmail.com>
2020-08-29 21:49:42 +0300

    unsigned minus int

---
+++ unsignedminusint.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned u1=42, u2=10;
+	cout<<"unsigned u1 (42) - u2 (10) = "<<u1-u2<<endl;
+	cout<<"u2 (10) - insigned u1 (42) = "<<u2-u1<<endl;
+}
